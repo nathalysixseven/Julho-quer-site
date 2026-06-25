1c13ec48781e1cac7632518bf68d5b41ab74d226
nathalysixseven <nathaly.fernandes31@escola.pr.gov.br>
2026-06-25 09:56:43 -0300

    colocar emoji

---
+++ script.js
@@ -0,0 +1,8 @@
+const curtidas = document.getElementById("curtidas") // pega o botão de curtidas pelo id
+curtidas.addEventListener("click", aumentarCurtias) // quando clicar chama a função 
+
+// função de aumentar curtidas 
+function aumentarCurtidas(){
+    let quantidade = document.querySelector9("span")
+    quntidade.textContentt++;
+}
